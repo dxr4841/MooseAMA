a8ea835254a8808b846306eb3f5a3e04ec5c159a
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 09:51:26 -0500

    add table for questions migration

--- db/migrate/20150128093550_create_questions.rb
+++ db/migrate/20150128093550_create_questions.rb
@@ -1,4 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.integer :user_topic_id
+      t.string  :content
+      t.integer :user_id #user that asks question
+      t.timestamps
+    end
   end
 end
