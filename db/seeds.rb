e4851b149be3cbb95f2d005fbbd4fe0ee69dc46d
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 16:09:15 -0500

    update seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
 6.times do
   User.create(username: Faker::Internet.user_name, email:Faker::Internet.email, password:"test" )
 end
-User.create(username: "test", email: "test@test.org", password: "test")
+test = User.create(username: "test", email: "test@test.org", password: "test")
+
+6.times do |id|
+  test.asked_questions.create(content: Faker::Lorem.sentence, replier_id: (id +1))
+end
+ 6.times do |id|
+  User.find(id+1).asked_questions.create(content: Faker::Lorem.sentence, replier_id: test.id)
+end
