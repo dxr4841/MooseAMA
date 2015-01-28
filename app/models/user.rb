f78825b32f7ae8569301062fcde656c306dcb82a
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 10:48:08 -0500

    add assocations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :questions
+  has_many :answers, through: :questions
 
+  has_many :asked_questions, foreign_key: "asker_id", class_name "Question"
+  has_many :replied_questions, foreign_key: "replier_id", class_name "Question"
+
+  has_many :askers, through: :replied_questions, source: :asker
+  has_many :repliers, through: :asked_questions, source: :replier
 end
