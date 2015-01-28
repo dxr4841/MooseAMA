ab5d2e972c5eab1fbf43009f7fdcb428ca88f819
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 14:34:22 -0500

    add methods to questions that gets the name for asker and replier

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :asker, class_name: "User"
   belongs_to :replier, class_name: "User"
+
+  def asker_name
+    User.find_by(id: self.asker_id).username
+  end
+
+  def replier_name
+    User.find_by(id: self.replier_id).username
+  end
 end
