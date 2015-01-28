08bcafa177a26085afcac868c0e55b1c2621bbdb
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 12:23:53 -0500

    add put route to users

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,3 +2,9 @@ get '/users/:id' do |id|
   @user = User.find(id)
   erb :"/user/show"
 end
+
+put '/users/:id' do |id|
+  @user = User.find(id)
+  @user.update(params[:user])
+  redirect "/users/:id"
+end
