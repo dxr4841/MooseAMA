8c7f98756e474a174918cdd947b9af38a7748a20
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 10:51:48 -0500

    fix typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers, through: :questions
 
-  has_many :asked_questions, foreign_key: "asker_id", class_name "Question"
-  has_many :replied_questions, foreign_key: "replier_id", class_name "Question"
+  has_many :asked_questions, foreign_key: "asker_id", class_name: "Question"
+  has_many :replied_questions, foreign_key: "replier_id", class_name: "Question"
 
   has_many :askers, through: :replied_questions, source: :asker
   has_many :repliers, through: :asked_questions, source: :replier
