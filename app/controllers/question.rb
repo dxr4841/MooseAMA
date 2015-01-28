9fafdf36e79edbdfe52a20ebc200b1330555bbf2
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 14:26:13 -0500

    add route for listing all questions

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,3 +1,8 @@
+get "/questions/all" do
+  @questions = Question.all
+  erb :'/question/all'
+end
+
 get "/questions/:id" do |id|
   @question = Question.find(id)
   erb :'/question/single', locals: {question: @question}
