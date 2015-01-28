5af36b890c00e463c394004ce75c8a9cb2e08160
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 10:32:50 -0500

    change questions migration to make question a join table between to users

--- db/migrate/20150128093550_create_questions.rb
+++ db/migrate/20150128093550_create_questions.rb
@@ -1,9 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.integer :user_topic_id
+      # t.integer :user_topic_id
       t.text  :content
-      t.integer :user_id #user that asks question
+      t.integer :asker_id #user that asks question
+      t.integer :replier_id # user that answers question
       t.timestamps
     end
   end
