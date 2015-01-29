2712a6f91bdb52b408921c3c7c70ef2792884d2b
Darrin Reeves <dxr4841@gmail.com>
2015-01-28 21:16:43 -0500

    access control

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,17 @@ configure do
   enable :sessions
   set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
 
+  register do
+    def auth(type)
+      condition do
+        unless send("current_#{type}")
+          redirect '/login'
+          add_error!("Not authorized, please login.")
+        end
+      end
+    end
+  end
+
   # Set the views to
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
